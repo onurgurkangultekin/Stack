13f677d5c43d681cec78ea2d3563544bb9d1957c
onurgurkangultekin <onurgurkangultekin@gmail.com>
2020-04-14 16:46:32 +0300

    stack implementation

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<string> banknotes = new Stack<string>();
+            Console.WriteLine("size: " + banknotes.Size());
+            banknotes.Push("200 TL");
+            banknotes.Push("100 TL");
+            banknotes.Push("50 TL");
+            banknotes.Push("20 TL");
+            banknotes.Push("10 TL");
+            banknotes.Push("5 TL");
+
+            Console.WriteLine("size: " + banknotes.Size());
+
+            foreach (var item in banknotes)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine(banknotes.Peak());
+            Console.WriteLine("size: " + banknotes.Size());
+
+            Console.WriteLine(banknotes.Pop());
+            Console.WriteLine(banknotes.Pop());
+            Console.WriteLine("size: " + banknotes.Size());
+
+            foreach (var item in banknotes)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private T[] data;
+        private int size;
+
+        public Stack(int size)
+        {
+            data = new T[size];
+        }
+        
+        public Stack() : this(1000)
+        {
+        }
+
+        public  void Push(T item)
+        {
+            if (size == data.Length)
+            {
+                throw new InvalidOperationException("no more space in the stack");
+            }
+
+            data[size++] = item;
+        }
+
+        public T Pop()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException("no item in the stack");
+            }
+
+            return data[--size];
+        }
+
+        public T Peak()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException("no item left in the stack");
+            }
+
+            return data[size - 1];
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = size - 1; i >= 0; i--)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
